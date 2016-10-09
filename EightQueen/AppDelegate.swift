f5eeb4d9c3b17edd74f96cdce23a0e48a9f4688d
leifjacky <leif.jacky@gmail.com>
2016-10-09 12:43:23 +0800

    Initial Commit

---
+++ EightQueen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EightQueen
+//
+//  Created by Leif on 09/10/2016.
+//  Copyright © 2016 Leif. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EightQueen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EightQueen
+//
+//  Created by Leif on 09/10/2016.
+//  Copyright © 2016 Leif. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
